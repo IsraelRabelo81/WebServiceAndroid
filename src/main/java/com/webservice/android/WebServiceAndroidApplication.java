d4c052640a6081e8017b038a0121092e16a416ae
Israel Rabelo <israelmanarslz@hotmail.com>
2020-10-08 21:17:47 -0300

    Projeto Criado

---
+++ src/main/java/com/webservice/android/WebServiceAndroidApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceAndroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceAndroidApplication.class, args);
+	}
+
+}
